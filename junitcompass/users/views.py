573cd68b3fb930ed67de0792429afb0dd43a32ed
Albina <alb.mslva@gmail.com>
2025-12-01 10:17:33 +0400

    feat: add user registration with form validation

--- junitcompass/users/views.py
+++ junitcompass/users/views.py
@@ -1,12 +1,25 @@
 from django.contrib.auth import  logout
 from django.contrib.auth.views import LoginView, LogoutView
 from django.http import  HttpResponseRedirect
+from django.shortcuts import render
 from django.urls import reverse
 
-from users.forms import LoginUserForm
+from users.forms import LoginUserForm, RegisterUserForm
 
 
 class LoginUser(LoginView):
     form_class = LoginUserForm
     template_name = 'users/login.html'
     extra_context = {'title': 'Авторизация'}
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            return render(request, 'users/register_done.html')
+    else:
+        form = RegisterUserForm()
+    return render(request, 'users/register.html', {'form': form})
